2ae94b1b83b18e7aefae7b0817ce09238b3916f9
IanYoo <thyoostar@gmail.com>
2020-08-02 23:40:41 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
+from __future__ import print_function
+
 from setuptools import find_packages, setup
 
 
+def get_long_description():
+    with open('README.md') as f:
+        long_description = f.read()
+
+    try:
+        import github2pypi
+
+        return github2pypi.replace_url(
+            slug='IanTaehoonYoo/semantic-segmentation-pytorch', content=long_description
+        )
+    except Exception:
+        return long_description
+
 setup(name="seg_torch",
-      version="0.1.0",
+      version="0.1.1",
       description="Semantic Segmentation with Pytorch",
+      long_description=get_long_description(),
+      long_description_content_type='text/markdown',
       author="Ian Yoo",
       author_email='thyoostar@gmail.com',
       platforms=["any"],
@@ -11,8 +28,9 @@ setup(name="seg_torch",
       url="https://github.com/IanTaehoonYoo/semantic-segmentation-pytorch",
       packages=find_packages(exclude=["segmentation/test/dataset"]),
       install_requires=[
-            "torch>=1.5.0",
-            "torchvision>=0.6.0",
+            "pytorch>=1.5.0",
+            "torchvision>=0.5.0",
+            "tensorboardX>=2.0"
             "opencv-python",
             "tqdm"],
       extras_require={
