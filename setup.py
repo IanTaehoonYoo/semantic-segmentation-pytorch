09fc03d42b9e48b2940fb4fc912d8e20b13d92b5
IanYoo <thyoostar@gmail.com>
2020-08-07 00:41:47 -0400

    setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_long_description():
         return long_description
 
 setup(name="seg_torch",
-      version="0.1.2",
+      version="0.1.4",
       description="Semantic Segmentation with Pytorch",
       long_description=get_long_description(),
       long_description_content_type='text/markdown',
